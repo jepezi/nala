692efe9a81b156d35b2c49df728192206d741d01
Jirat Arinrith <jepezi@gmail.com>
2016-03-02 08:20:07 +0700

    load env in index.js for server

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 require('babel-polyfill')
 
+var dotenv = require('dotenv')
+dotenv.load({
+  path: process.cwd() + '/.env',
+  silent: true
+})
+
 global.__CLIENT__ = false;
 global.__SERVER__ = true;
 global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
